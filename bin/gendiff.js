3957d1ec2b9dc1d391a834f7fbfb95e1dbf5ecb7
super-gr0ver <e.dorodnov@gmail.com>
2023-03-28 00:43:05 +0300

    fix

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,2 +1,16 @@
 #!/usr/bin/env node
-console.log('Hello')
+import { Command } from "../node_modules/commander/esm.mjs";
+// import genDiff from '../src/index.js';
+
+const program = new Command();
+
+program
+  .version('0.1.0')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filepath1, filepath2, options) => {
+    console.log(genDiff(filepath1, filepath2, options.format));
+  });
+
+program.parse(process.argv);
