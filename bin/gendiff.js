e69a45572cf8b8dbd5ae6a5e0bccf4003025b051
super-gr0ver <e.dorodnov@gmail.com>
2023-03-30 01:35:31 +0300

    fix

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,16 +1,27 @@
 #!/usr/bin/env node
-import { program } from "../node_modules/commander/esm.mjs";
+import { program } from '../node_modules/commander/esm.mjs';
+import * as fs from 'fs';
 // import genDiff from '../src/index.js';
 
 // const program = new Command();
 
-program
-  .version('0.1.0')
-  .description('Compares two configuration files and shows a difference.')
-  .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format', 'stylish')
-  .action((filepath1, filepath2, options) => {
-    console.log(genDiff(filepath1, filepath2, options.format));
-  });
+// program
+//   .version('0.1.0') 
+//   .description('Compares two configuration files and shows a difference.')
+//   .arguments('<filepath1> <filepath2>')
+//   .option('-f, --format <type>', 'output format', 'stylish')
+//   .action((filepath1, filepath2, options) => {
+//     console.log(genDiff(filepath1, filepath2, options.format));
+//   });
   
-program.parse(process.argv);
+// program.parse(process.argv);
+
+function fileHandler(){
+  const str = JSON.parse(fs.readFileSync('file1.json', 'utf8'));
+  return console.log(str.host);
+
+  
+  
+}
+
+fileHandler()
