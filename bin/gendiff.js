d26dd9e9fabd7ed44148fa8b76498b8472682697
super-gr0ver <e.dorodnov@gmail.com>
2023-04-06 00:27:07 +0300

    fix

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,44 +1,41 @@
 #!/usr/bin/env node
-import { program } from '../node_modules/commander/esm.mjs';
-import * as fs from 'fs';
-import _ from 'lodash';
+import { program } from "../node_modules/commander/esm.mjs";
+import * as fs from "fs";
+import _ from "lodash";
 
 // import genDiff from '../src/index.js';
 
 // const program = new Command();
 
 program
-  .version('0.1.0') 
-  .description('Compares two configuration files and shows a difference.')
-  .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format', 'stylish')
+  .version("0.1.0")
+  .description("Compares two configuration files and shows a difference.")
+  .arguments("<filepath1> <filepath2>")
+  .option("-f, --format <type>", "output format", "stylish")
   .action((filepath1, filepath2, options) => {
-    const objFile1 = JSON.parse(fs.readFileSync(filepath1, 'utf8'));
-    const objFile2 = JSON.parse(fs.readFileSync(filepath2, 'utf8'));
+    const objFile1 = JSON.parse(fs.readFileSync(filepath1, "utf8"));
+    const objFile2 = JSON.parse(fs.readFileSync(filepath2, "utf8"));
+    const keysObjFiles = _.sortBy(Object.keys({ ...objFile1, ...objFile2 }));
 
-    const arrFile1 = Object.entries(objFile1);
-    const arrFile2 = Object.entries(objFile2);
-    //console.log(objFile1)
-
-    const result = []
-    for (const [key, value] of arrFile1) {
-      // if (_.includes(arrFile2, 'host')) {
-      
-      if (_.has(objFile2, key) && objFile2[key] === value) {
-        result.push(`  ${key}: ${value}`);
+    const result = [];
+    for (const currentKey of keysObjFiles) {
+      if (objFile1[currentKey] === objFile2[currentKey]) {
+        result.push(`    ${currentKey}: ${objFile1[currentKey]}`);
+      } else if (_.has(objFile1, currentKey) && _.has(objFile2, currentKey)) {
+        result.push(
+          `  - ${currentKey}: ${objFile1[currentKey]}\n  + ${currentKey}: ${objFile2[currentKey]}`
+        );
       }
-      result.push(`- ${key}: ${value}`);
+
+      if (_.has(objFile1, currentKey) && !_.has(objFile2, currentKey)) {
+        result.push(`  - ${currentKey}: ${objFile1[currentKey]}`);
+      }
+
+      if (!_.has(objFile1, currentKey) && _.has(objFile2, currentKey)) {
+        result.push(`  + ${currentKey}: ${objFile2[currentKey]}`);
+      }
     }
-
-    return console.log(`{\n${result.join('\n')}\n}`)
-    
-    // console.log(genDiff(filepath1, filepath2, options.format));
+    return console.log(`{\n${result.join("\n")}\n}`)
   });
-  
-program.parse(process.argv);
 
-// function fileHandler(){
-//   const str = JSON.parse(fs.readFileSync('file1.json', 'utf8'));
-//   return console.log(str.host);
-// }
-// fileHandler()
+program.parse(process.argv);
