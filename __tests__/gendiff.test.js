7b10948ef7accf911d45e0fb3727293522fa8af7
super-gr0ver <e.dorodnov@gmail.com>
2023-04-10 23:12:37 +0300

    fix

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,10 @@
+import { test, expect } from '@jest/globals';
+import gendiff from '../bin/gendiff.js';
+import fs from 'fs';
+import path from 'path'
+
+
+// test('gendiff', () => {
+//   expect(reverse('hello')).toEqual('olleh');
+//   expect(reverse('')).toEqual('');
+// });
