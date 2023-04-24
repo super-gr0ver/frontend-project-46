380494fddb99543f0e64a8614a90f4881fcb804f
super-gr0ver <e.dorodnov@gmail.com>
2023-04-25 00:35:14 +0300

    fix

--- src/tree-compar.js
+++ src/tree-compar.js
@@ -1,11 +1,11 @@
 import _ from 'lodash';
 
-const getKeys =  (file1Date, file2Date) => {
+const getKeys = (file1Date, file2Date) => {
   const keys = Object.keys({ ...file1Date, ...file2Date });
-  return _.sortBy(keys)
-} 
+  return _.sortBy(keys);
+};
 const genDiffTree = (file1Date, file2Date) => {
-  const keysObjFiles = getKeys(file1Date, file2Date)
+  const keysObjFiles = getKeys(file1Date, file2Date);
   const result = keysObjFiles.reduce((acc, key) => {
     if (file1Date[key] === file2Date[key]) {
       acc.push(`    ${key}: ${file1Date[key]}`);
