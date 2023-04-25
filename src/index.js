824f3e41c0d9ab9a0812706838117233a38336dc
super-gr0ver <e.dorodnov@gmail.com>
2023-04-25 21:48:39 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import fs from 'fs';
 import path from 'path';
 import genDiffTree from './tree-compar.js';
+import parseObj from './parse.js';
 
 const getFilePath = (fileName) => path.resolve(process.cwd(), fileName);
-const readFile = (filePath) => JSON.parse(fs.readFileSync(filePath, 'utf8'));
+const readFile = (filePath) => fs.readFileSync(getFilePath(filePath), 'utf8');
+const getFormatFile = (filePath) => {
+  const [, ...result] = filePath.split('.');
+  return result.toString();
+};
 
 const genDiff = (file1, file2) => {
-  const file1Path = getFilePath(file1);
-  const file1Date = readFile(file1Path);
+  const file1Data = readFile(file1);
+  const parseObj1 = parseObj(file1Data, getFormatFile(file1));
 
-  const file2Path = getFilePath(file2);
-  const file2Date = readFile(file2Path);
+  const file2Data = readFile(file2);
+  const parseObj2 = parseObj(file2Data, getFormatFile(file2));
 
-  return genDiffTree(file1Date, file2Date);
+  return genDiffTree(parseObj1, parseObj2);
 };
 
 export default genDiff;
