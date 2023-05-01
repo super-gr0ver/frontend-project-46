2727e078dbfff8b0c3b091ac22c47a7866ead1ea
super-gr0ver <e.dorodnov@gmail.com>
2023-05-01 03:25:00 +0300

    fix

--- src/stylish.js
+++ src/stylish.js
@@ -1,33 +1,47 @@
 import _ from 'lodash';
 
+const replacer = ' ';
+const spaceCount = 4;
+
+const currentIndent = (depth) => {
+  const replacerCount = depth * spaceCount;
+  return replacer.repeat(replacerCount);
+};
+const indent = (depth) => {
+  const replacerCount = depth * spaceCount;
+  return replacer.repeat(replacerCount - 2);
+};
+
+
 const nodeChilds = (node, depth) => {
   if (!_.isPlainObject(node)) {
     return `${node}`;
   }
   const result = Object.entries(node).map(
-    ([key, value]) => `${key}: ${nodeChilds(value, depth + 1)}`
+    ([key, value]) =>
+      `${currentIndent(depth + 1)}${key}: ${nodeChilds(value, depth + 1)}`
   );
-  return `{\n${result.join('\n')}\n}`;
+  return `{\n${result.join('\n')}\n${currentIndent(depth)}}`;
 };
 
-const iter = (diffTree, depth) => {
-  const replacer = ' ';
-  const spaceCount = 4;
-  const replacerCount = depth * spaceCount;
-  const currentIndent = replacer.repeat(replacerCount);
 
+const iter = (diffTree, depth) => {
   const valueObj = Object.values(diffTree).map((node) => {
     switch (node.status) {
       case 'deleted':
-        return `${currentIndent}- ${node.name}: ${nodeChilds(node.value, depth)}`;
+        return `${indent(depth)}- ${node.name}: ${nodeChilds(
+          node.value,
+          depth
+        )}`;
       case 'added':
-        return `${currentIndent}+ ${node.name}: ${nodeChilds(node.value, depth)}`;
+        return `${indent(depth)}+ ${node.name}: ${nodeChilds(node.value,depth)}`;
       case 'changed':
-        return `${currentIndent}- ${node.name}: ${nodeChilds(node.oldValue, depth)}\n${currentIndent}+ ${node.name}: ${nodeChilds(node.newValue, depth)}`;
+        return `${indent(depth)}- ${node.name}: ${nodeChilds(node.oldValue,depth
+        )}\n${indent(depth)}+ ${node.name}: ${nodeChilds(node.newValue,depth)}`;
       case 'unchanged':
-        return `  ${currentIndent}${node.name}: ${nodeChilds(node.value, depth)}`;
+        return `${currentIndent(depth)}${node.name}: ${nodeChilds(node.value,depth)}`;
       case 'nested':
-        return `${node.name}: ${iter(node.value, depth)}`;
+        return `${currentIndent(depth)}${node.name}: ${iter(node.value, depth)}`;
     }
   });
   return `{\n${valueObj.join('\n')}\n}`;
