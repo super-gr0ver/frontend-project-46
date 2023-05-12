4fc53cfa465f496089dc9c6f23e0975e64f8fcd7
super-gr0ver <e.dorodnov@gmail.com>
2023-05-12 22:10:23 +0300

    FIX

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,4 +1,3 @@
-import yaml from 'js-yaml';
 import stylish from './stylish.js';
 import plain from './plain.js';
 
@@ -6,8 +5,6 @@ const dataFormat = (data, format) => {
   switch (format) {
     case 'stylish':
       return stylish(data);
-    case 'plain':
-      return plain(data);
     case 'json':
       return JSON.stringify(data, null);
     case 'yml':
